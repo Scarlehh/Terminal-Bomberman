157955fc320dff8b5c2366ce092701c9780db2fb
Dário Tavares Antunes <tavaresd@tcd.ie>
2016-11-23 20:03:27 +0000

    Added bomb cloning and freeing functions.

--- include/bomb.h
+++ include/bomb.h
@@ -7,11 +7,15 @@
 #define BOMB_TIMER_START 0 * TICKS_PER_SECOND
 #define BOMB_TIMER_END 3 * TICKS_PER_SECOND
 
+#define BOMB_DISPLAY '@'
+
 struct Bomb {
 	int strength;
 	int timer;
 };
 
 void init_bomb(struct Bomb *);
+struct Bomb * clone_bomb(struct Bomb *);
+void free_bomb(struct Bomb *);
 
 #endif
--- src/bomb.c
+++ src/bomb.c
@@ -1,6 +1,20 @@
+#include <stdlib.h>
 #include "bomb.h"
 
 void init_bomb(struct Bomb * bomb) {
 	bomb->strength = BOMB_STRENGTH_START;
 	bomb->timer = BOMB_TIMER_START;
+}
+
+struct Bomb * clone_bomb(struct Bomb * toClone) {
+	struct Bomb * clone = malloc(sizeof(struct Bomb));
+
+	clone->strength = toClone->strength;
+	clone->timer = toClone->timer;
+
+	return clone;
+}
+
+void free_bomb(struct Bomb * bomb) {
+	free(bomb);
 }
