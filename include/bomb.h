9498d6cbe03f707436897071bb72ca43c4ab6691
Dário Tavares Antunes <tavaresd@tcd.ie>
2016-11-23 19:19:45 +0000

    Added bomb timers for use in the physics loop.

--- include/bomb.h
+++ include/bomb.h
@@ -1,8 +1,11 @@
 #ifndef BOMB_H
 #define BOMB_H
 
+#include "physics.h"
+
 #define BOMB_STRENGTH_START 3
-#define BOMB_TIMER_START 0
+#define BOMB_TIMER_START 0 * TICKS_PER_SECOND
+#define BOMB_TIMER_END 3 * TICKS_PER_SECOND
 
 struct Bomb {
 	int strength;
