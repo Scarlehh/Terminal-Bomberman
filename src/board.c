4c0e21a080671c6225199273d6aafe62dbf10663
Scarlett <gourleys@tcd.ie>
2016-12-14 17:02:49 +0000

    Added function to clear directions around a r c pair

--- src/board.c
+++ src/board.c
@@ -9,7 +9,7 @@ struct Board * new_board(int height, int width) {
 	board->height = height;
 
 	for (int r = 0; r < board->height; r++) {
-		for (int c = 0; c < board->height; c++) {
+		for (int c = 0; c < board->width; c++) {
 			struct Square * sq = get_square(board, r, c);
 			sq->c = c;
 			sq->r = r;
@@ -33,12 +33,51 @@ void clear_board(struct Board * board) {
 	}
 }
 
+void clear_sides(struct Board* board, int r, int c) {
+	struct Square* sq;
+	if(r > 0) {
+		sq = get_square(board, r-1, c);
+		sq->display = ' ';
+		sq->type = EMPTY;
+		sq->data = NULL;
+	}
+	if(r+1 < board->height) {
+		sq = get_square(board, r+1, c);
+		sq->display = ' ';
+		sq->type = EMPTY;
+		sq->data = NULL;
+	}
+	if(c > 0) {
+		sq = get_square(board, r, c-1);
+		sq->display = ' ';
+		sq->type = EMPTY;
+		sq->data = NULL;
+	}
+	if(c+1 < board->width) {
+		sq = get_square(board, r, c+1);
+		sq->display = ' ';
+		sq->type = EMPTY;
+		sq->data = NULL;
+	}
+}
+
 void generate_walls(struct Board* board) {
-	for(int r = 1; r < board->height; r+=2) {
-		for(int c = 1; c < board->width; c+=2) {
+	time_t t;
+	/* Intializes random number generator */
+	srand((unsigned) time(&t));
+
+	for(int r = 0; r < board->height; r++) {
+		for(int c = 0; c < board->width; c++) {
 			struct Square* sq = get_square(board, r, c);
-			sq->type = BREAKABLE;
-			sq->display = '#';
+			// Leave perimeter open
+			if((r+1)%2==0 && (c+1)%2 == 0) {
+				sq->type = BLOCK;
+				sq->display = '#';
+			}
+			else if(rand()%3 < 1) {
+				sq->type = BREAKABLE;
+				sq->display = '#';
+			}
 		}
 	}
 }
