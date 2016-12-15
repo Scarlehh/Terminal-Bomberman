983181b01844209a9adcd3fa389dcf28a0881071
Scarlett <gourleys@tcd.ie>
2016-12-15 15:50:01 +0000

    Timer placeholders

--- include/physics.h
+++ include/physics.h
@@ -8,6 +8,13 @@
 #define NANOS_PER_TICK NANOS_PER_SECOND / TICKS_PER_SECOND
 #define MELT_TIMER 1 * TICKS_PER_SECOND
 
+struct Timer {
+	int timer;
+	struct Square* nextSquare;
+};
+
+struct Timer* init_timer(struct Square* nextSquare);
+void delete_timer(struct Timer* timer);
 int start_physics(struct Board * board);
 int stop_physics();
 
